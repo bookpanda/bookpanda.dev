df8cc5dd65e01e552c503eafa0bb3f0df536d106
Idhibhat Pankam <marginpankam@gmail.com>
2023-03-31 23:11:31 +0700

    feat: add links to profile

--- apps/client/public/images/index.ts
+++ apps/client/public/images/index.ts
@@ -3,3 +3,6 @@ export { default as thai2kana } from "./thai2kana.png";
 export { default as autoPraeAksorn } from "./autoPraeAksorn.png";
 export { default as aniTimeline } from "./aniTimeline.png";
 export { default as remuNekoka } from "./remuNekoka.gif";
+export { default as anilist } from "./anilist.svg";
+export { default as github } from "./github.svg";
+export { default as linkedin } from "./linkedin.svg";
--- apps/client/src/pages/about.tsx
+++ apps/client/src/pages/about.tsx
@@ -1,6 +1,9 @@
 import Layout from "$modules/Layout/Layout";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { anilist, github, linkedin } from "public/images";
 
 const AboutPage: NextPage = () => {
   return (
@@ -9,10 +12,44 @@ const AboutPage: NextPage = () => {
         <Typography variant="h3" fontWeight={600}>
           Who am I?
         </Typography>
-        <Typography variant="h6" fontWeight={300}>
+        <Typography variant="h6" fontWeight={300} marginBottom={10}>
           I am Idhibhat Pankam, currently a freshmen at Computer Engineering,
           Chulalongkorn University.
         </Typography>
+        <Typography variant="h4" fontWeight={600}>
+          Links
+        </Typography>
+        <div className="flex items-center justify-evenly lg:w-1/3">
+          <Link
+            href={"https://github.com/bookpanda"}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <IconButton sx={{ marginTop: 2 }} size="large">
+              <Image alt="github" src={github} height={40} width={40} />
+            </IconButton>
+          </Link>
+          <Link
+            href={"https://www.linkedin.com/in/idhibhat-pankam"}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <IconButton sx={{ marginTop: 2 }} size="large">
+              <Image alt="linkedin" src={linkedin} height={400} width={100} />
+            </IconButton>
+          </Link>
+          <Link
+            href={"https://anilist.co/user/bookpanda"}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <IconButton sx={{ marginTop: 2 }} size="large">
+              <div className="bg-gray-800">
+                <Image alt="anilist" src={anilist} height={40} width={40} />
+              </div>
+            </IconButton>
+          </Link>
+        </div>
       </div>
     </Layout>
   );
