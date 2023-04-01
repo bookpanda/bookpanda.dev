fb7341c0647aa929bdaa8568f511914c2b273c2e
Idhibhat Pankam <marginpankam@gmail.com>
2023-04-02 00:07:34 +0700

    feat: responsive anime calendar

--- apps/client/src/pages/hobbies.tsx
+++ apps/client/src/pages/hobbies.tsx
@@ -5,10 +5,12 @@ import {
   WatchCalendar,
 } from "$modules/WatchCalendar";
 import { WatchingAnime } from "$modules/WatchingAnime";
-import { Typography } from "@mui/material";
+import { Typography, useMediaQuery } from "@mui/material";
 import { NextPage } from "next";
+import { theme } from "src/theme";
 
 const HobbiesPage: NextPage = () => {
+  const breakXL = useMediaQuery(theme.breakpoints.up("xl"));
   return (
     <Layout header={<p>Hobbies</p>}>
       <div className="mb-20">
@@ -17,13 +19,15 @@ const HobbiesPage: NextPage = () => {
         </Typography>
         <WatchingAnime />
       </div>
-      <div className="h-[60vh] w-full">
-        <WatchCalendar />
-        <div className="mt-8 flex h-[40vh] w-full justify-evenly">
-          <SelectYear />
-          <DisplayAnime />
+      {breakXL && (
+        <div className="h-[70vh] w-full">
+          <WatchCalendar />
+          <div className="mt-8 flex h-[50vh] w-full justify-evenly">
+            <SelectYear />
+            <DisplayAnime />
+          </div>
         </div>
-      </div>
+      )}
     </Layout>
   );
 };
