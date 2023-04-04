c3e21cf8beab6f7d8530fcb7a93a9817e134a807
Idhibhat Pankam <marginpankam@gmail.com>
2023-04-04 17:41:26 +0700

    feat: favicon

--- apps/client/src/pages/about.tsx
+++ apps/client/src/pages/about.tsx
@@ -1,15 +1,22 @@
 import { AboutMe, Links } from "$modules/About";
 import { Layout } from "$modules/Layout";
 import { NextPage } from "next";
+import Head from "next/head";
 
 const AboutPage: NextPage = () => {
   return (
-    <Layout header={<p>About me</p>}>
-      <div className="">
-        <AboutMe />
-        <Links />
-      </div>
-    </Layout>
+    <>
+      <Head>
+        <title>About me</title>
+        <meta content="App description" name="description" />
+      </Head>
+      <Layout header={<p>About me</p>}>
+        <div className="">
+          <AboutMe />
+          <Links />
+        </div>
+      </Layout>
+    </>
   );
 };
 
--- apps/client/src/pages/apis.tsx
+++ apps/client/src/pages/apis.tsx
@@ -1,14 +1,21 @@
 import { Layout } from "$modules/Layout";
 import { Typography } from "@mui/material";
 import { NextPage } from "next";
+import Head from "next/head";
 
 const APIPage: NextPage = () => {
   return (
-    <Layout header={<p>APIs</p>}>
-      <div className="h-[70vh]">
-        <Typography color="secondary.dark">Coming Soon...</Typography>
-      </div>
-    </Layout>
+    <>
+      <Head>
+        <title>APIs</title>
+        <meta content="App description" name="description" />
+      </Head>
+      <Layout header={<p>APIs</p>}>
+        <div className="h-[70vh]">
+          <Typography color="secondary.dark">Coming Soon...</Typography>
+        </div>
+      </Layout>
+    </>
   );
 };
 
