55789d858899947f38471a0c8b834509b3ae0ea2
Idhibhat Pankam <marginpankam@gmail.com>
2024-05-12 23:00:53 +0700

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+import clsx from "clsx";
+import Link from "next/link";
+import styles from "./styles.module.css";
+
 const Home = () => {
   return (
-    <div className="bg-red-100">
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+    <div className={clsx("h-screen w-screen", styles.gradientBg)}>
+      <div className="flex h-full w-full flex-col items-end justify-center pr-[5%]">
+        <h1 className="text-lg text-white">
+          <Link href="https://www.bookpanda.dev/">bookpanda.dev</Link>
+        </h1>
+        <p className="text-white">
+          <Link href="https://resume.bookpanda.dev/" target="_blank">
+            resume
+          </Link>
+        </p>
+        <p className="text-white">
+          <Link href="/projects" target="_blank">
+            projects
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
