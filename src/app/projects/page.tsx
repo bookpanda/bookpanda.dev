4f5072319c46e4b4cd99e35bcb057ccf35324bc8
Idhibhat Pankam <marginpankam@gmail.com>
2024-05-13 23:26:21 +0700

    feat: layout projs

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { translate } from "@/cms/Translate";
+import { texts } from "@/cms/texts";
+import { Footer } from "@/components/Footer/Footer";
 import { Navbar } from "@/components/Navbar/Navbar";
 import clsx from "clsx";
 import styles from "./styles.module.css";
 
 const ProjectsPage = () => {
   return (
-    <div className={clsx("relative h-screen w-screen", styles.gradientBg)}>
-      <Navbar title="Projects" />
+    <div className={clsx("relative w-screen", styles.gradientBg)}>
+      <Navbar title={translate(texts.home.projects)} />
+      <div className="mx-[10%] my-[5vh] h-[150vh] bg-red-100"></div>
+      <Footer />
     </div>
   );
 };
