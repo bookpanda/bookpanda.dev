c588eb34a3a1cd4ca4b787aeb2578bec21966b47
Idhibhat Pankam <marginpankam@gmail.com>
2024-05-14 12:16:59 +0700

    fix: overlay padding

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -13,14 +13,14 @@ interface ProjectProps {
 export const Project: FC<ProjectProps> = ({ project }) => {
   return (
     <div className="relative w-full rounded-md bg-[#3a3a3a70] drop-shadow-xl">
-      <div className="absolute flex h-full w-full flex-col justify-between rounded-md bg-[#000000b4] p-2 opacity-0 duration-300 ease-in-out hover:opacity-100">
+      <div className="absolute flex h-full w-full flex-col justify-between rounded-md bg-[#000000b4] p-4 opacity-0 duration-300 ease-in-out hover:opacity-100">
         <div>
           <h2 className="mb-4 text-xl text-white">{project.title}</h2>
           <p className="text-md text-white">{project.description}</p>
         </div>
-        <div className="flex w-full justify-end pb-4 pr-4 hover:cursor-pointer">
+        <div className="flex w-full justify-end pb-4 pr-4">
           <Link href={project.url} target="_blank">
-            <FaGithub className="h-8 w-8 text-white" />
+            <FaGithub className="h-8 w-8 text-white hover:cursor-pointer" />
           </Link>
         </div>
       </div>
